b9a78fab1920d04a99cab909244af9bfc2e465fa
Shubham Gupta <shubhamgupto@gmail.com>
2024-03-29 00:10:52 -0400

    clang

--- python/setup.py
+++ python/setup.py
@@ -89,8 +89,9 @@ def get_llvm_package_info():
     # release_suffix = "assert" if use_assert_enabled_llvm else "release"
     llvm_hash_file = open("../cmake/llvm-hash.txt", "r")
     rev = llvm_hash_file.read(8)
-    name = f"llvm-{rev}-{system_suffix}"
-    url = f"https://tritonlang.blob.core.windows.net/llvm-builds/{name}.tar.gz"
+    name = "clang+llvm-17.0.1-aarch64-linux-gnu"
+    # url = f"https://tritonlang.blob.core.windows.net/llvm-builds/{name}.tar.gz"
+    url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.1/clang+llvm-17.0.1-aarch64-linux-gnu.tar.xz"
     return Package("llvm", name, url, "LLVM_INCLUDE_DIRS", "LLVM_LIBRARY_DIR", "LLVM_SYSPATH")
 
 
